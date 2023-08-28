d9bc3783ddd8ba29b6b86ecb19b39aa9191c1276
advanced373 <mihaistoica1998@gmail.com>
2023-08-28 23:13:02 +0300

    #1 Add basic stucture for login/register feature.

--- syt-frontend/src/pages/main.tsx
+++ syt-frontend/src/pages/main.tsx
@@ -1,11 +1,13 @@
+import { AuthenticationForm } from "../features/authentication/authentication.component";
 import { ThoughtFormComponent } from "../features/thought-create/thought-form.component";
 import { ListThoughtsComponent } from "../features/thought-list/thought-list.component";
 
 export const MainPage = () =>{
     return (
         <>
-        <ThoughtFormComponent></ThoughtFormComponent>
-        <ListThoughtsComponent></ListThoughtsComponent>
+        {/* <ThoughtFormComponent></ThoughtFormComponent>
+        <ListThoughtsComponent></ListThoughtsComponent> */}
+        <AuthenticationForm withBorder={true} w={"50%"} m={"auto"}></AuthenticationForm>
         </>
     
     );
